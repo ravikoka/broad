1b4df473bd03e8ad08ae346b7d80188d5cc2e2bc
ravikoka <ravikkoka@gmail.com>
2024-04-29 12:48:37 -0500

    initial plots for thesis

---
+++ thesis/fit_comparison.py
@@ -0,0 +1,219 @@
+import ROOT as rt
+import numpy as np
+import matplotlib.pyplot as plt
+
+rt.gStyle.SetOptFit(1111) # show fit panel w/ fit prob, chi2/ndf, fit params, errors
+
+import sys
+sys.path.append('../classes/')
+sys.path.append('../utils/')
+
+from SparseAnalyzer import SparseAnalyzer #, FitFunction, FitRange, CurveFit
+from formatting import process_pave, process_tgraph, process_hist
+
+from CurveFit import FitFunction, FitRange, CurveFit
+
+EPSILON = 0.0001
+
+# load the ROOT file
+inFile = rt.TFile.Open("../dists/analysisMultiFine.root")
+
+# load single particle histograms, TH3D
+#   axes are: pT, phi, eta
+allParticleDist = inFile.allParticleDist
+chargedHadronDist = inFile.chargedHadronDist
+lambdaDist = inFile.lambdaDist
+triggeredLambdaDist = inFile.triggeredLambdaDist
+
+# load correlation dists, 6D THnSparse 
+#   axes are: trigger eta, associated eta, trigger pt, associated pt, delta phi, delta eta
+hlDist = inFile.hLambdaDist
+hhDist = inFile.hhDist
+
+hh = SparseAnalyzer(hhDist, name='hh', apply_cut=False)
+hh08 = SparseAnalyzer(hhDist, name='hh08', etaAssoc=[-0.8, 0.8-EPSILON], apply_cut=True)
+hh12 = SparseAnalyzer(hhDist, name='hh12', etaAssoc=[-1.2, 1.2-EPSILON], apply_cut=True)
+hh20 = SparseAnalyzer(hhDist, name='hh20', etaAssoc=[-2.0, 2.0-EPSILON], apply_cut=True)
+
+hl = SparseAnalyzer(hlDist, name='hl', apply_cut=False)
+hl08 = SparseAnalyzer(hlDist, name='hl08', etaAssoc=[-0.8, 0.8-EPSILON], apply_cut=True)
+hl12 = SparseAnalyzer(hlDist, name='hl12', etaAssoc=[-1.2, 1.2-EPSILON], apply_cut=True)
+hl20 = SparseAnalyzer(hlDist, name='hl20', etaAssoc=[-2.0, 2.0-EPSILON], apply_cut=True)
+
+
+# fit to regular gaussian
+distributions = [hh08, hh12, hh20, hl08, hl12, hl20]
+
+c_gaus = rt.TCanvas('gaus_canvas', 'gaus_canvas')
+c_gaus.SetCanvasSize(2400, 800)
+c_gaus.Divide(3, 2)
+
+c_gen_gaus = rt.TCanvas('gaus_gen_canvas', 'gaus_gen_canvas')
+c_gen_gaus.SetCanvasSize(2400, 800)
+c_gen_gaus.Divide(3, 2)
+
+c_mises = rt.TCanvas('mises_canvas', 'gaus_mises_canvas')
+c_mises.SetCanvasSize(2400, 800)
+c_mises.Divide(3, 2)
+
+fit_functions=[FitFunction.double_gaussian, FitFunction.double_generalized_gaussian, FitFunction.double_von_mises]
+
+gaus_params = []
+gen_gaus_params = []
+mises_params = []
+
+pi = rt.TMath.Pi()
+gaus_guesses =[[20000, 0.0, 1, 8000, pi, 1], [20000, 0.0, 1, 15000, pi, 1], [20000, 0.0, 1, 20000, pi, 1],
+                [100, 0.0, 1, 150, pi, 1], [100, 0.0, 0.19, 250, pi, 1], [100, 0.0, 1, 400, pi, 1]]
+
+gen_gaus_guesses = [[0.0, 0.7, 1.8, 1, pi, 1, 1.8, 1], [0.0, 0.7, 1.8, 1, pi, 1, 1.8, 1], [0.0, 0.7, 1.8, 1, pi, 1, 1.8, 1], [0.0, 0.7, 1.8, 1, pi, 1, 1.8, 1],
+                        [0.0, 0.7, 1.8, 1, pi, 1, 1.8, 1], [0.0, 0.7, 1.8, 1, pi, 1, 1.8, 1], [0.0, 0.7, 1.8, 1, pi, 1, 1.8, 1], [0.0, 0.7, 1.8, 1, pi, 1, 1.8, 1]]
+
+mises_guesses = [[20000, 0.0, 1, 8000, pi, 1], [20000, 0.0, 1, 15000, pi, 1], [20000, 0.0, 30, 20000, pi, 1],
+                [100, 0.0, 1, 150, pi, 1], [100, 0.0, 0.19, 250, pi, 1], [100, 0.0, 1, 400, pi, 1]]
+
+for i, (sparse, guess) in enumerate(zip(distributions, gaus_guesses)):
+        c_gaus.cd(i + 1) 
+
+        gaussian = CurveFit(sparse=sparse, dphi=sparse.make_delta_phi(), fit_function=FitFunction.double_gaussian, fit_range=FitRange.full_range, fit_params=guess)
+        dphi, params = gaussian.fit()
+        
+        gaus_params.append(params)
+        dphi.Draw()
+
+for i, (sparse, guess) in enumerate(zip(distributions, gen_gaus_guesses)):
+        c_gen_gaus.cd(i + 1) 
+
+        gen_gaussian = CurveFit(sparse=sparse, dphi=sparse.make_delta_phi(), fit_function=FitFunction.double_generalized_gaussian, fit_range=FitRange.full_range, fit_params=guess)
+        dphi, params = gen_gaussian.fit()
+
+        gen_gaus_params.append(params)
+        dphi.Draw()
+
+for i, (sparse, guess) in enumerate(zip(distributions, mises_guesses)):
+        c_mises.cd(i + 1) 
+
+        mises = CurveFit(sparse=sparse, dphi=sparse.make_delta_phi(), fit_function=FitFunction.double_von_mises, fit_range=FitRange.full_range, fit_params=guess)
+        dphi, params = mises.fit()
+        mises_params.append(params)
+        dphi.Draw()
+
+c_gaus.SaveAs('gaussian_fit.pdf')
+c_gen_gaus.SaveAs('gen_gaussian_fit.pdf')
+c_mises.SaveAs('mises_fit.pdf')
+
+
+
+gaus_params = np.array(gaus_params)
+gaus_stdev_near = gaus_params[:, 2, 0]
+gaus_stdev_away = gaus_params[:, 5, 0]
+
+def gen_gaus_stdev(alpha, beta):
+    '''
+    Returns the standard deviation of a generalized gaussian, given alpha and beta. 
+    '''
+    var = alpha**2 * rt.Math.tgamma(3 / beta) / rt.Math.tgamma(1 / beta)
+    
+    return rt.TMath.Sqrt(var)
+
+gen_gaus_params = np.array(gen_gaus_params)
+
+gen_gaus_params_alpha_near = gen_gaus_params[:, 1, 0]
+gen_gaus_params_beta_near = gen_gaus_params[:, 2, 0] 
+
+gen_gaus_params_alpha_away = gen_gaus_params[:, 5, 0]
+gen_gaus_params_beta_away = gen_gaus_params[:, 6, 0]
+
+gen_gaus_stdev_near = np.array([gen_gaus_stdev(alpha, beta) for (alpha, beta) in zip(gen_gaus_params_alpha_near, gen_gaus_params_beta_near)])
+gen_gaus_stdev_away = np.array([gen_gaus_stdev(alpha, beta) for (alpha, beta) in zip(gen_gaus_params_alpha_away, gen_gaus_params_beta_away)])
+
+
+def mises_stdev(kappa):
+    '''
+    Returns the standard deviation of a von mises distribution, given alpha and beta.
+    '''
+    var = 1 - rt.TMath.BesselI1(kappa) / rt.TMath.BesselI0(kappa)
+    var *= 2
+    #var = -2 * rt.TMath.Log(rt.TMath.BesselI1(kappa) / rt.TMath.BesselI0(kappa))
+
+    return rt.TMath.Sqrt(var)
+
+mises_params = np.array(mises_params)
+mises_kappa_near = mises_params[:, 2, 0]
+mises_kappa_away = mises_params[:, 5, 0]
+
+mises_stdev_near = np.array([mises_stdev(kappa) for kappa in mises_kappa_near])
+mises_stdev_away = np.array([mises_stdev(kappa) for kappa in mises_kappa_away])
+
+
+plt.rcParams['figure.dpi'] = 400
+
+fig = plt.figure()
+gs = fig.add_gridspec(2, 2, hspace=0, wspace=0.1)
+((ax1, ax2),(ax3, ax4)) = gs.subplots(sharex='col', sharey='row')
+
+eta = [0.8, 1.2, 2.0]
+ax1.scatter(eta, gaus_stdev_near[:3], color='k')
+ax1.scatter(eta, gen_gaus_stdev_near[:3], color='b')
+ax1.scatter(eta, mises_stdev_near[:3], color='r')
+
+ax2.scatter(eta, gaus_stdev_near[3:], color='k', label='Gaussian')
+ax2.scatter(eta, gen_gaus_stdev_near[3:], color='b', label='Gen Gaussian')
+ax2.scatter(eta, mises_stdev_near[3:], color='r',  label='Von Mises')
+
+ax3.scatter(eta, gaus_stdev_away[:3], color='k')
+ax3.scatter(eta, gen_gaus_stdev_away[:3], color='b')
+ax3.scatter(eta, mises_stdev_away[:3], color='r')
+
+ax4.scatter(eta, gaus_stdev_away[3:], color='k')
+ax4.scatter(eta, gen_gaus_stdev_away[3:], color='b')
+ax4.scatter(eta, mises_stdev_away[3:], color='r')
+
+ax3.set_xlim(0.6, 2.2)
+ax4.set_xlim(0.6, 2.2)
+ax3.set_xticks(eta)
+ax4.set_xticks(eta)
+
+ax1.set_ylabel('$\sigma_{NS}$')
+ax3.set_ylabel('$\sigma_{AS}$')
+
+ax3.set_xlabel('$|\eta|<x$')
+ax4.set_xlabel('$|\eta|<x$')
+
+ax1.set_title('h-h')
+ax2.set_title('h-$\Lambda$')
+
+ax2.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+plt.tight_layout()
+plt.savefig('widths-across-fits.pdf')
+
+
+fig, axs = plt.subplots(2, sharex=True)
+
+eta = [0.8, 1.2, 2.0]
+ns_stdevs = [gaus_stdev_near, gen_gaus_stdev_near, mises_stdev_near]
+as_stdevs = [gaus_stdev_away, gen_gaus_stdev_away, mises_stdev_away]
+colors = ['k', 'b', 'r']
+labels = ['Guassian', 'Gen Gaussian', 'Von Mises']
+
+for ns_stdev, as_stdev, color, label in zip(ns_stdevs, as_stdevs, colors, labels):
+    near_ratios = ns_stdev[3:] / ns_stdev[:3] # h-L / h-h near side
+    away_ratios = as_stdev[3:] / as_stdev[:3] # h-L / h-h away side
+
+    axs[0].scatter(eta, near_ratios, color=color, label=label)
+    axs[1].scatter(eta, away_ratios, color=color, label=label)
+
+axs[0].set_xticks(eta)
+axs[1].set_xticks(eta)
+
+axs[0].legend()
+
+axs[1].set_xlabel('$|\eta|<x$')
+axs[0].set_ylabel('$\sigma^{h-\Lambda}_{NS} / \sigma^{h-h}_{NS}$')
+axs[1].set_ylabel('$\sigma^{h-\Lambda}_{AS} / \sigma^{h-h}_{AS}$')
+
+fig.suptitle('Width Ratios for Various Fits and $\eta$ Cuts')
+
+plt.tight_layout()
+plt.savefig('ratios-across-fits.pdf')
---
+++ thesis/plots.py
@@ -0,0 +1,318 @@
+import ROOT as rt
+import numpy as np
+import matplotlib.pyplot as plt
+
+rt.gStyle.SetOptFit(1111) # show fit panel w/ fit prob, chi2/ndf, fit params, errors
+
+import sys
+sys.path.append('../classes/')
+sys.path.append('../utils/')
+
+from SparseAnalyzer import SparseAnalyzer #, FitFunction, FitRange, CurveFit
+from formatting import process_pave, process_tgraph, process_hist
+
+from CurveFit import FitFunction, FitRange, CurveFit
+
+EPSILON = 0.0001
+
+# load the ROOT file
+inFile = rt.TFile.Open("../dists/analysisMultiFine.root")
+
+# load single particle histograms, TH3D
+#   axes are: pT, phi, eta
+allParticleDist = inFile.allParticleDist
+chargedHadronDist = inFile.chargedHadronDist
+lambdaDist = inFile.lambdaDist
+triggeredLambdaDist = inFile.triggeredLambdaDist
+
+# load correlation dists, 6D THnSparse 
+#   axes are: trigger eta, associated eta, trigger pt, associated pt, delta phi, delta eta
+hlDist = inFile.hLambdaDist
+hhDist = inFile.hhDist
+
+
+hh = SparseAnalyzer(hhDist, name='hh', apply_cut=False)
+hh08 = SparseAnalyzer(hhDist, name='hh08', etaAssoc=[-0.8, 0.8-EPSILON], apply_cut=True)
+hh12 = SparseAnalyzer(hhDist, name='hh12', etaAssoc=[-1.2, 1.2-EPSILON], apply_cut=True)
+hh20 = SparseAnalyzer(hhDist, name='hh20', etaAssoc=[-2.0, 2.0-EPSILON], apply_cut=True)
+
+hl = SparseAnalyzer(hlDist, name='hl', apply_cut=False)
+hl08 = SparseAnalyzer(hlDist, name='hl08', etaAssoc=[-0.8, 0.8-EPSILON], apply_cut=True)
+hl12 = SparseAnalyzer(hlDist, name='hl12', etaAssoc=[-1.2, 1.2-EPSILON], apply_cut=True)
+hl20 = SparseAnalyzer(hlDist, name='hl20', etaAssoc=[-2.0, 2.0-EPSILON], apply_cut=True)
+
+distributions = [hh, hh08, hh12, hh20, hl, hl08, hl12, hl20]
+
+##########################
+## Width Extraction
+##########################
+
+# standard gaussian fit
+dPhiCanvas = rt.TCanvas('dphi_canvas', 'dphi_canvas')
+
+dPhiCanvas.SetCanvasSize(2400, 800)
+dPhiCanvas.Divide(4, 2)
+
+guesses = [[15000, 0.2, 10000, 0.2], [20000, 1, 8000, 1], [20000, 1, 15000, 1], [20000, 1, 20000, 1],
+           [200, 1, 800, 1], [100, 1, 150, 1], [100, 0.19, 250, 1], [70, 1, 400, 1]]
+awayStDev = np.zeros(shape=(len(distributions), 2))
+nearStDev = np.zeros(shape=(len(distributions), 2))
+for i, (sparse, guess) in enumerate(zip(distributions, guesses)):
+    dPhiCanvas.cd(i + 1)
+
+    dPhi = sparse.make_delta_phi()
+    dPhi, params = sparse.fit_delta_phi(dPhi, guess, ratio_plot=False)
+    
+    nearStDev[i] = params[2]
+    awayStDev[i] = params[5]
+    print(params[2], params[5])
+    print(nearStDev)
+    
+
+    pave = sparse.make_pave()    
+    dPhi.Draw()
+    pave.Draw()
+        
+dPhiCanvas.SaveAs('delta_phi_plots.png')
+
+#############################
+## Width Ratios Calculations
+#############################
+
+# plot ratio of yields in near side and away side, for same type of correlation
+widthsNearOverAwayCanvas = rt.TCanvas()
+widthsNearOverAwayCanvas.SetCanvasSize(800, 400)
+
+# get rid of ratios for distributions with no eta cuts
+filter = [False, True, True, True, 
+          False, True, True, True]
+
+nearStDev = nearStDev[filter]
+awayStDev = awayStDev[filter]
+
+# get widths for h-h azimuthal correlations
+hhAwayWidths = awayStDev[:3, 0]
+hhNearWidths = nearStDev[:3, 0]
+hhAwayErrs = awayStDev[:3, 1]
+hhNearErrs = nearStDev[:3, 1]
+
+# get widths for h-Lambda azimuthal correlations
+hlAwayWidths = awayStDev[3:, 0]
+hlNearWidths = nearStDev[3:, 0]
+hlAwayErrs = awayStDev[3:, 1]
+hlNearErrs = nearStDev[3:, 1]
+
+# typical error propagation for ratio
+errorProp = lambda x, xerr, y, yerr: (x / y) * np.sqrt((xerr / x)**2 + (yerr / y)**2)
+
+# calc h-h near side width / h-h away side width
+# hhRatios = hhNearWidths / hhAwayWidths
+# hhErrs = errorProp(hhNearWidths, hhNearErrs, hhAwayWidths, hhAwayErrs)
+hhRatios = hhAwayWidths / hhNearWidths
+hhErrs = errorProp(hhAwayWidths, hhAwayErrs, hhNearWidths, hhNearErrs)
+
+# calc h-Lambda near side width / h-h near side width
+# hlRatios = hlNearWidths / hlAwayWidths
+# hlErrs = errorProp(hlNearWidths, hlNearErrs, hlAwayWidths, hlAwayErrs)
+hlRatios = hlAwayWidths / hlNearWidths
+hlErrs = errorProp(hlAwayWidths, hlAwayErrs, hlNearWidths, hlNearErrs)
+
+# calc h-Lambda away side width / h-h away side width
+widthRatios = hlAwayWidths / hhAwayWidths
+ratioErrs = errorProp(hlAwayWidths, hlAwayErrs, hhAwayWidths, hhAwayErrs)
+# ratioErrs = widthRatios * np.sqrt(1 / hlAwayWidths + 1 / hhAwayWidths)
+
+print(widthRatios, ratioErrs)
+
+etaAxis = np.array([0.8, 1.2, 2.0], dtype='d')
+etaErrs = np.zeros(len(etaAxis))
+
+
+##########################
+## Away-Side Width Ratios
+##########################
+
+widthRatiosCanvas = rt.TCanvas('widthRatiosCanvas', 'widthRatiosCanvas')
+rt.gPad.SetLeftMargin(0.19)
+rt.gPad.SetBottomMargin(0.13)
+
+widthRatiosPave = rt.TPaveText(0.4, 0.7, 0.799425, 0.860169, 'NDC') 
+widthRatiosPave.AddText('pp, PYTHIA6, #sqrt{s}=14 TeV')
+widthRatiosPave.AddText('4 < p_{T}^{trig} < 8 GeV/c, 2 < p_{T}^{assoc} < 4 GeV/c')
+widthRatiosPave.AddText('|#eta^{trig}| < 0.8')
+process_pave(widthRatiosPave, size=0.04)#0.06)
+
+
+widthRatiosGraph = rt.TGraphErrors(len(widthRatios), etaAxis, widthRatios, ex=etaErrs, ey=ratioErrs)
+widthRatiosGraph.SetMarkerStyle(107)
+
+widthRatiosGraph.SetTitle('Away-side Width Ratio per #eta cut')
+#widthRatiosGraph.GetYaxis().SetTitle('Width Ratio #left(#frac{h-#Lambda}{h-h}#right)')
+widthRatiosGraph.GetYaxis().SetTitle('#sigma^{h-#Lambda}_{AS} / #sigma^{h-h}_{AS}')
+widthRatiosGraph.GetXaxis().SetTitle('|#eta^{assoc}| < x')
+
+process_tgraph(widthRatiosGraph)
+
+widthRatiosGraph.GetYaxis().SetRangeUser(0.8, 1.2)
+#widthRatiosGraph.GetYaxis().LabelsOption('v')
+
+line = rt.TLine(0.68, 1.0, 2.12, 1.0)
+line.SetLineColor(4)
+line.SetLineStyle(2)
+line.SetLineWidth(2)
+
+widthRatiosGraph.Draw('ap')
+widthRatiosPave.Draw()
+line.Draw()
+widthRatiosCanvas.Draw()
+widthRatiosCanvas.SaveAs('away_side_width_ratios.pdf')
+
+
+##########################
+## Near-Side Width Ratios
+##########################
+
+nsWidthRatiosCanvas = rt.TCanvas('nsWidthRatiosCanvas', 'nsWidthRatiosCanvas')
+rt.gPad.SetLeftMargin(0.19)
+rt.gPad.SetBottomMargin(0.13)
+
+nsWidthRatios = hlNearWidths / hhNearWidths
+nsWidthErrs = errorProp(hlNearWidths, hlNearErrs, hhNearWidths, hhNearErrs)
+
+nsWidthRatiosGraph = rt.TGraphErrors(len(etaAxis), etaAxis, nsWidthRatios, ex=etaErrs, ey=nsWidthErrs)
+nsWidthRatiosGraph.SetMarkerStyle(21)
+nsWidthRatiosGraph.SetMarkerColor(4)
+nsWidthRatiosGraph.SetLineColor(4)
+
+nsWidthRatiosGraph.SetTitle('Near-side Width Ratio per #eta cut')
+nsWidthRatiosGraph.GetYaxis().SetTitle('#sigma^{h-#Lambda}_{NS} / #sigma^{h-h}_{NS}')
+nsWidthRatiosGraph.GetXaxis().SetTitle('|#eta^{assoc}| < x')
+
+process_tgraph(nsWidthRatiosGraph)
+nsWidthRatiosGraph.SetMaximum(1.5)
+nsWidthRatiosGraph.SetMinimum(0.2)
+
+nsWidthRatiosGraph.Draw('ap')
+widthRatiosPave.Draw()
+nsWidthRatiosCanvas.Draw()
+nsWidthRatiosCanvas.SaveAs('near_side_width_ratios.pdf')
+
+
+###############################
+## Away-Over-Near Width Ratios
+###############################
+
+#hhRatiosCanvas = rt.TCanvas()
+#rt.gPad.SetLeftMargin(0.19)
+#rt.gPad.SetBottomMargin(0.13)
+
+hhRatiosPave = widthRatiosPave.Clone()
+
+hhRatiosGraph = rt.TGraphErrors(len(hhRatios), etaAxis, hhRatios, ex=etaErrs, ey=hhErrs)
+hhRatiosGraph.SetTitle('h-h Width Ratios per #eta Cut')
+#hhRatiosGraph.GetYaxis().SetTitle('h-h Width Ratios #left(#frac{NS}{AS}#right)')
+hhRatiosGraph.GetYaxis().SetTitle('#sigma^{h-h}_{AS} / #sigma^{h-h}_{NS}')
+hhRatiosGraph.GetXaxis().SetTitle('|#eta^{assoc}| < x')
+
+hhRatiosGraph.SetMarkerStyle(20)
+hhRatiosGraph.SetMarkerColor(2)
+hhRatiosGraph.SetLineColor(2)
+
+#process_tgraph(hhRatiosGraph)
+
+#hhRatiosGraph.SetMinimum(0)
+#hhRatiosGraph.SetMaximum(2.3)
+
+#hRatiosGraph.Draw('ap')
+#hhRatiosPave.Draw()
+#hhRatiosCanvas.Draw()
+
+#hlRatiosCanvas = rt.TCanvas()
+#rt.gPad.SetLeftMargin(0.19)
+#rt.gPad.SetBottomMargin(0.13)
+
+hlRatiosPave = widthRatiosPave.Clone()
+
+hlRatiosGraph = rt.TGraphErrors(len(hlRatios), etaAxis, hlRatios, ex=etaErrs, ey=hlErrs)
+hlRatiosGraph.SetTitle('h-#Lambda width ratios per #eta cut')
+hlRatiosGraph.GetXaxis().SetTitle('|#eta^{assoc}| < x')
+#hlRatiosGraph.GetYaxis().SetTitle('h-#Lambda Width Ratios #left(#frac{NS}{AS}#right)')
+hlRatiosGraph.GetYaxis().SetTitle('#sigma^{h-#Lambda}_{AS} / #sigma^{h-#Lambda}_{NS}')
+
+#process_tgraph(hlRatiosGraph)
+
+hlRatiosGraph.SetMinimum(0.0)
+hlRatiosGraph.SetMaximum(2.5)
+
+
+hlRatiosGraph.SetMarkerStyle(21)
+#hlRatiosGraph.Draw('ap')
+#hlRatiosPave.Draw()
+#hlRatiosCanvas.Draw()
+
+
+compareRatiosCanvas = rt.TCanvas()
+rt.gPad.SetLeftMargin(0.19)
+rt.gPad.SetBottomMargin(0.13)
+
+widthsMultiGraph = rt.TMultiGraph('widthmg', 'widthmg')
+
+#hhRatiosGraph.SetMarkerColor(2)
+#hlRatiosGraph.SetMarkerColor(1)
+
+#hhRatiosGraph.SetMarkerStyle(8)
+#hlRatiosGraph.SetMarkerStyle(21)
+
+
+hhRatiosGraph.SetTitle('#sigma^{h-h}_{AS} / #sigma^{h-h}_{NS}')
+hlRatiosGraph.SetTitle('#sigma^{h-#Lambda}_{AS} / #sigma^{h-#Lambda}_{NS}')
+widthsMultiGraph.Add(hhRatiosGraph, 'p')
+widthsMultiGraph.Add(hlRatiosGraph, 'p')
+
+
+widthsMultiGraph.SetTitle('Comparison of Widths Ratios per #eta Cut')
+widthsMultiGraph.GetXaxis().SetTitle('|#eta^{assoc}| < x')
+#widthsMultiGraph.GetYaxis().SetTitle('Width Ratios #left(#frac{NS}{AS}#right)')
+widthsMultiGraph.GetYaxis().SetTitle('#sigma_{AS} / #sigma_{NS}')
+widthsMultiGraph.GetYaxis().SetRangeUser(1.2, 2.5)
+
+process_tgraph(widthsMultiGraph)
+
+widthsMultiGraph.Draw('ap')
+
+legend = compareRatiosCanvas.BuildLegend(0.571839, 0.68644, 0.87212, 0.89618)
+legend.SetBorderSize(0)
+legend.SetFillStyle(0)
+legend.SetTextSize(0.035)
+
+widthsMultiGraphPave = rt.TPaveText(0.5, 0.760593, 0.899425, 0.860169)#0.189655, 0.764830, 0.589080, 0.86440677, 'NDC')
+widthsMultiGraphPave.AddText('4 < p_{T}^{trig} < 8 GeV/c, 2 < p_{T}^{assoc} < 4 GeV/c')
+widthsMultiGraphPave.AddText('|#eta^{trig}| < 0.8')
+widthsMultiGraphPave = process_pave(widthRatiosPave, size=0.06)
+
+# set coordinates of pave and legend, ROOT ignores the previous coords for reasons unknown to me
+rt.gPad.Update()
+widthsMultiGraphPave.SetX1NDC(0.1896551724137931)
+widthsMultiGraphPave.SetY1NDC(0.760593220338983)
+widthsMultiGraphPave.SetX2NDC(0.5890804597701149)
+widthsMultiGraphPave.SetY2NDC(0.8601694915254238)
+
+legend.SetX1NDC(0.6681034482758621)
+legend.SetY1NDC(0.6949152542372881)
+legend.SetX2NDC(0.9683908045977011)
+legend.SetY2NDC(0.9046610169491526)
+rt.gPad.Modified()
+
+widthsMultiGraphPave.Draw()
+compareRatiosCanvas.Draw()
+compareRatiosCanvas.SaveAs('away-over-near.pdf')
+
+
+###############################
+## Away-Over-Near Width Ratios
+###############################
+
+
+
+
+
